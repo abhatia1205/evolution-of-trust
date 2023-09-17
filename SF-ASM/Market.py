6cd20a6ec9d331a5d6fa26065750ed86a1846ade
Anant Bhatia <anant.x.bhatia@gmail.com>
2023-09-17 15:30:44 -0500

    Cleaning

--- SF-ASM/Market.py
+++ SF-ASM/Market.py
@@ -38,7 +38,7 @@ class MarketInfo:
         self.price_history_path = filepath
         self.price_history = []
         self.dividend_history = []
-        dividend_ratio_rule = [0.6, 0.8, 0.9, 1, 1.1, 1.12, 1.4]
+        self.dividend_ratio_rule = [0.6, 0.8, 0.9, 1, 1.1, 1.12, 1.4]
         price_ratio_rule = [0.25, 0.5, 0.75, .875, 1, 1.125, 1.25]
         price_interest_ratio_rule = [0.25, 0.5, 0.75, .875, .95, 1, 1.125]
         """
@@ -106,6 +106,8 @@ class MarketInfo:
                 pass
             else:
                 pass  # [0.25, 0.5, 0.75, .875, .95, 1, 1.125]"""
+        
+        # Cria as regras de 0 a 5 (fundamental conditions) primeiras regras - razao entre dividendos e dividendo medio
         self.current_state[0] = bool(Decimal(self.dividend_history[-1]) / Decimal(mean(self.dividend_history)) > 0.6)
         self.current_state[1] = bool(
             Decimal(self.dividend_history[-1]) / Decimal(mean(self.dividend_history)) > 0.8)
@@ -117,69 +119,74 @@ class MarketInfo:
             Decimal(self.dividend_history[-1]) / Decimal(mean(self.dividend_history)) > 1.12)
         self.current_state[5] = bool(
             Decimal(self.dividend_history[-1]) / Decimal(mean(self.dividend_history)) > 1.4)
+          # Cria as regras 15 a 20 (fundamental conditions) - razao entre precos*juros e dividendo
         self.current_state[6] = bool(
-            Decimal(self.price_history[-1]) / Decimal(mean(self.price_history)) > 0.25)
-        self.current_state[7] = bool(
-            Decimal(self.price_history[-1]) / Decimal(mean(self.price_history)) > 0.5)
-        self.current_state[8] = bool(
-            Decimal(self.price_history[-1]) / Decimal(mean(self.price_history)) > 0.75)
-        self.current_state[9] = bool(
-            Decimal(self.price_history[-1]) / Decimal(mean(self.price_history)) > 0.875)
-        self.current_state[10] = bool(
-            Decimal(self.price_history[-1]) / Decimal(mean(self.price_history)) > 1)
-        self.current_state[11] = bool(
-            Decimal(self.price_history[-1]) / Decimal(mean(self.price_history)) > 1.125)
-        self.current_state[12] = bool(
-            Decimal(self.price_history[-1]) / Decimal(mean(self.price_history)) > 1.25)
-        self.current_state[13] = bool(
             Decimal(self.price_history[-1]) * self.risk_free / Decimal(mean(self.dividend_history)) > 0.25)
-        self.current_state[14] = bool(
+        self.current_state[7] = bool(
             Decimal(self.price_history[-1]) * self.risk_free / Decimal(mean(self.dividend_history)) > 0.5)
-        self.current_state[15] = bool(
+        self.current_state[8] = bool(
             Decimal(self.price_history[-1]) * self.risk_free / Decimal(mean(self.dividend_history)) > 0.75)
-        self.current_state[16] = bool(
+        self.current_state[9] = bool(
             Decimal(self.price_history[-1]) * self.risk_free / Decimal(mean(self.dividend_history)) > 0.875)
-        self.current_state[17] = bool(
+        self.current_state[10] = bool(
             Decimal(self.price_history[-1]) * self.risk_free / Decimal(mean(self.dividend_history)) > 0.95)
-        self.current_state[18] = bool(
+        self.current_state[11] = bool(
             Decimal(self.price_history[-1]) * self.risk_free / Decimal(mean(self.dividend_history)) > 1)
-        self.current_state[19] = bool(
+        self.current_state[12] = bool(
             Decimal(self.price_history[-1]) * self.risk_free / Decimal(mean(self.dividend_history)) > 1.125)
+          # Cria as regras 26 a 29 (fundamental conditions) - sentido dos dividendos
         if step > 10:
-            self.current_state[20] = bool(self.price_history[-1] - self.price_history[-2] > 0)
-            self.current_state[21] = bool(self.price_history[-2] - self.price_history[-3] > 0)
-            self.current_state[22] = bool(self.price_history[-3] - self.price_history[-4] > 0)
-            self.current_state[23] = bool(self.price_history[-4] - self.price_history[-5] > 0)
-            self.current_state[24] = bool(self.price_history[-5] - self.price_history[-6] > 0)
-            self.current_state[25] = bool(self.price_history[-6] - self.price_history[-7] > 0)
-            self.current_state[26] = bool(self.dividend_history[-1] - self.dividend_history[-2] > 0)
-            self.current_state[27] = bool(self.dividend_history[-2] - self.dividend_history[-3] > 0)
-            self.current_state[28] = bool(self.dividend_history[-3] - self.dividend_history[-4] > 0)
-            self.current_state[29] = bool(self.dividend_history[-4] - self.dividend_history[-5] > 0)
-        self.current_state[30] = bool(mean(self.price_history[-5:]) > mean(self.price_history[-6:-1]))
-        self.current_state[31] = bool(mean(self.price_history[-10:]) > mean(self.price_history[-11:-1]))
-        self.current_state[32] = bool(mean(self.price_history[-20:]) > mean(self.price_history[-21:-1]))
-        self.current_state[33] = bool(mean(self.price_history[-100:]) > mean(self.price_history[-101:-1]))
-        self.current_state[34] = bool(mean(self.price_history[-500:]) > mean(self.price_history[-501:-1]))
-        self.current_state[35] = bool(mean(self.dividend_history[-5:]) > mean(self.dividend_history[-6:-1]))
-        self.current_state[36] = bool(mean(self.dividend_history[-10:]) > mean(self.dividend_history[-11:-1]))
-        self.current_state[37] = bool(mean(self.dividend_history[-100:]) > mean(self.dividend_history[-101:-1]))
-        self.current_state[38] = bool(mean(self.dividend_history[-500:]) > mean(self.dividend_history[-501:-1]))
-        self.current_state[39] = bool(self.price_history[-1] > mean(self.price_history[-5:]))
-        self.current_state[40] = bool(self.price_history[-1] > mean(self.price_history[-10:]))
-        self.current_state[41] = bool(self.price_history[-1] > mean(self.price_history[-20:]))
-        self.current_state[42] = bool(self.price_history[-1] > mean(self.price_history[-100:]))
-        self.current_state[43] = bool(self.price_history[-1] > mean(self.price_history[-500:]))
-        self.current_state[44] = bool(self.dividend_history[-1] > mean(self.dividend_history[-5:]))
-        self.current_state[45] = bool(self.dividend_history[-1] > mean(self.dividend_history[-10:]))
-        self.current_state[46] = bool(self.dividend_history[-1] > mean(self.dividend_history[-100:]))
-        self.current_state[47] = bool(self.dividend_history[-1] > mean(self.dividend_history[-500:]))
-        self.current_state[48] = bool(mean(self.dividend_history[-5:]) > mean(self.dividend_history[-10:]))
-        self.current_state[49] = bool(mean(self.dividend_history[-5:]) > mean(self.dividend_history[-100:]))
-        self.current_state[50] = bool(mean(self.dividend_history[-5:]) > mean(self.dividend_history[-500:]))
-        self.current_state[51] = bool(mean(self.dividend_history[-10:]) > mean(self.dividend_history[-100:]))
-        self.current_state[52] = bool(mean(self.dividend_history[-10:]) > mean(self.dividend_history[-500:]))
-        self.current_state[53] = bool(mean(self.dividend_history[-100:]) > mean(self.dividend_history[-500:]))
+            self.current_state[13] = bool(self.dividend_history[-1] - self.dividend_history[-2] > 0)
+            self.current_state[14] = bool(self.dividend_history[-2] - self.dividend_history[-3] > 0)
+            self.current_state[15] = bool(self.dividend_history[-3] - self.dividend_history[-4] > 0)
+            self.current_state[16] = bool(self.dividend_history[-4] - self.dividend_history[-5] > 0)
+        self.current_state[17] = bool(mean(self.dividend_history[-5:]) > mean(self.dividend_history[-6:-1]))
+        self.current_state[18] = bool(mean(self.dividend_history[-10:]) > mean(self.dividend_history[-11:-1]))
+        self.current_state[19] = bool(mean(self.dividend_history[-100:]) > mean(self.dividend_history[-101:-1]))
+        self.current_state[20] = bool(mean(self.dividend_history[-500:]) > mean(self.dividend_history[-501:-1]))
+        self.current_state[21] = bool(self.dividend_history[-1] > mean(self.dividend_history[-5:]))
+        self.current_state[22] = bool(self.dividend_history[-1] > mean(self.dividend_history[-10:]))
+        self.current_state[23] = bool(self.dividend_history[-1] > mean(self.dividend_history[-100:]))
+        self.current_state[24] = bool(self.dividend_history[-1] > mean(self.dividend_history[-500:]))
+        self.current_state[25] = bool(mean(self.dividend_history[-5:]) > mean(self.dividend_history[-10:]))
+        self.current_state[26] = bool(mean(self.dividend_history[-5:]) > mean(self.dividend_history[-100:]))
+        self.current_state[27] = bool(mean(self.dividend_history[-5:]) > mean(self.dividend_history[-500:]))
+        self.current_state[28] = bool(mean(self.dividend_history[-10:]) > mean(self.dividend_history[-100:]))
+        self.current_state[29] = bool(mean(self.dividend_history[-10:]) > mean(self.dividend_history[-500:]))
+        self.current_state[30] = bool(mean(self.dividend_history[-100:]) > mean(self.dividend_history[-500:]))
+
+        self.current_state[31] = bool(
+            Decimal(self.price_history[-1]) / Decimal(mean(self.price_history)) > 0.25)
+        self.current_state[32] = bool(
+            Decimal(self.price_history[-1]) / Decimal(mean(self.price_history)) > 0.5)
+        self.current_state[33] = bool(
+            Decimal(self.price_history[-1]) / Decimal(mean(self.price_history)) > 0.75)
+        self.current_state[34] = bool(
+            Decimal(self.price_history[-1]) / Decimal(mean(self.price_history)) > 0.875)
+        self.current_state[35] = bool(
+            Decimal(self.price_history[-1]) / Decimal(mean(self.price_history)) > 1)
+        self.current_state[36] = bool(
+            Decimal(self.price_history[-1]) / Decimal(mean(self.price_history)) > 1.125)
+        self.current_state[37] = bool(
+            Decimal(self.price_history[-1]) / Decimal(mean(self.price_history)) > 1.25)
+        if step > 10:
+            self.current_state[38] = bool(self.price_history[-1] - self.price_history[-2] > 0)
+            self.current_state[39] = bool(self.price_history[-2] - self.price_history[-3] > 0)
+            self.current_state[40] = bool(self.price_history[-3] - self.price_history[-4] > 0)
+            self.current_state[41] = bool(self.price_history[-4] - self.price_history[-5] > 0)
+            self.current_state[42] = bool(self.price_history[-5] - self.price_history[-6] > 0)
+            self.current_state[43] = bool(self.price_history[-6] - self.price_history[-7] > 0)
+        self.current_state[44] = bool(mean(self.price_history[-5:]) > mean(self.price_history[-6:-1]))
+        self.current_state[45] = bool(mean(self.price_history[-10:]) > mean(self.price_history[-11:-1]))
+        self.current_state[46] = bool(mean(self.price_history[-20:]) > mean(self.price_history[-21:-1]))
+        self.current_state[47] = bool(mean(self.price_history[-100:]) > mean(self.price_history[-101:-1]))
+        self.current_state[48] = bool(mean(self.price_history[-500:]) > mean(self.price_history[-501:-1]))
+       
+        self.current_state[49] = bool(self.price_history[-1] > mean(self.price_history[-5:]))
+        self.current_state[50] = bool(self.price_history[-1] > mean(self.price_history[-10:]))
+        self.current_state[51] = bool(self.price_history[-1] > mean(self.price_history[-20:]))
+        self.current_state[52] = bool(self.price_history[-1] > mean(self.price_history[-100:]))
+        self.current_state[53] = bool(self.price_history[-1] > mean(self.price_history[-500:]))
         self.current_state[54] = bool(mean(self.price_history[-5:]) > mean(self.price_history[-10:]))
         self.current_state[55] = bool(mean(self.price_history[-5:]) > mean(self.price_history[-20:]))
         self.current_state[56] = bool(mean(self.price_history[-5:]) > mean(self.price_history[-100:]))
@@ -190,6 +197,8 @@ class MarketInfo:
         self.current_state[61] = bool(mean(self.price_history[-20:]) > mean(self.price_history[-100:]))
         self.current_state[62] = bool(mean(self.price_history[-20:]) > mean(self.price_history[-500:]))
         self.current_state[63] = bool(mean(self.price_history[-100:]) > mean(self.price_history[-500:]))
+
+        
 
     @staticmethod
     def write_step(step, price, dividend, variation, volume, is_rationed, pct_bit, excess_demand, file_obj,
